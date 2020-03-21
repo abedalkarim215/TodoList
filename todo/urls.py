7309b0322d1f2f2f64acc793f3dd2db90bd6edf2
abedalkarim99 <abedalkarim215@gmail.com>
2020-03-22 00:34:40 +0200

    show the todos that complated(done) for the authenticated user

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('not_done' , todo_not_done ,name = 'todo_not_done'),
+    path('done' , todo_done ,name = 'todo_done'),
 
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -10,3 +10,10 @@ def todo_not_done(request) :
         'todos' : todos
     }
     return render(request,'todo/todo_not_done.html',context)
+
+def todo_done(request) :
+    todos = Todo.objects.filter(user=request.user, date_completed__isnull=False)
+    context = {
+        'todos': todos
+    }
+    return render(request, 'todo/todo-done.html',context)
