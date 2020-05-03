fb256fffa262f96b55372375edaff406c51e1550
abedalkarim99 <abedalkarim215@gmail.com>
2020-05-03 17:26:20 +0300

    change the style of the whole project and add translate for the arabic Language

--- todo/views.py
+++ todo/views.py
@@ -2,40 +2,71 @@ from django.shortcuts import render ,redirect ,get_object_or_404
 from .models import *
 from django.utils import timezone
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext as _
 
 
 def home(request) :
     return render(request,'todo/layouts/base-todo.html')
 
+
+
+@login_required(login_url='login_user')
 def todo_not_done(request) :
     todos = Todo.objects.filter(user=request.user, date_completed__isnull=True)
+    all_clear = False
+    if todos.__len__() == 0:
+        all_clear = True
     context = {
-        'todos' : todos
+        'todos' : todos,
+        'all_clear' : all_clear,
     }
     return render(request,'todo/todo_not_done.html',context)
 
+
+@login_required(login_url='login_user')
 def todo_done(request) :
     todos = Todo.objects.filter(user=request.user, date_completed__isnull=False)
+    all_clear = False
+    if todos.__len__() == 0:
+        all_clear = True
     context = {
-        'todos': todos
+        'todos': todos,
+        'all_clear': all_clear,
     }
     return render(request, 'todo/todo-done.html',context)
 
+
+
+@login_required(login_url='login_user')
 def add_todo(request) :
     if request.method == "GET" :
         return render(request,'todo/add-todo.html')
     else :
         title = request.POST['title']
         description = request.POST['description']
-        is_important = "is_important" in request.POST
-        todo = Todo.objects.create(title=title,
+        if request.POST["is_important"] == "YES":
+            is_important = True
+        elif request.POST["is_important"] == "NO":
+            is_important = False
+        if title == "" :
+            messages.info(request," please add a title")
+            context = {
+                'description' : description,
+                'is_important' : is_important,
+            }
+            return render(request,'todo/add-todo.html',context)
+        else:
+            todo = Todo.objects.create(title=title,
                                    description=description,
                                    is_important=is_important,
                                    user=request.user)
-        todo.save()
-        return redirect('todo_not_done')
+            todo.save()
+            return redirect('todo_not_done')
 
+
+
+@login_required(login_url='login_user')
 def show_todo(request ,todo_id) :
     if request.method == "GET" :
         todo = get_object_or_404(Todo,pk =todo_id,user = request.user)
@@ -44,6 +75,9 @@ def show_todo(request ,todo_id) :
         }
         return render(request,'todo/show_todo.html',context)
 
+
+
+@login_required(login_url='login_user')
 def edit_todo(request,todo_id) :
     todo = get_object_or_404(Todo, pk=todo_id, user=request.user)
     if request.method == "GET" :
@@ -54,7 +88,10 @@ def edit_todo(request,todo_id) :
     else :
         todo.title = request.POST['title']
         todo.description = request.POST['description']
-        todo.is_important = "is_important" in request.POST
+        if request.POST["is_important"] == "YES" :
+            todo.is_important = True
+        elif request.POST["is_important"] == "NO" :
+            todo.is_important = False
         todo.save()
         context = {
             "todo": todo,
@@ -62,15 +99,26 @@ def edit_todo(request,todo_id) :
         return render(request,'todo/show_todo.html',context)
 
 
+
+
+@login_required(login_url='login_user')
 def delete_todo(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id, user=request.user)
-    if request.method == 'POST':
+    if request.method == "GET" :
+        context = {
+            'todo' : todo,
+        }
+        return render(request,'todo/delete_todo.html',context)
+    elif request.method == 'POST':
         todo.delete()
         if todo.date_completed is not None :
             return redirect('todo_done')
         else :
             return redirect('todo_not_done')
 
+
+
+@login_required(login_url='login_user')
 def complate_todo(request,todo_id) :
     todo = get_object_or_404(Todo, pk=todo_id, user=request.user)
     if request.method == "POST" :
@@ -83,8 +131,13 @@ def complate_todo(request,todo_id) :
             todo.save()
             return redirect('todo_done')
 
+
+
+@login_required(login_url='login_user')
 def clear_todo(request) :
-    if request.method == "POST" :
+    if request.method == "GET" :
+        return render(request,'todo/clear.html')
+    elif request.method == "POST" :
         if "COM" in request.POST :
             todo = Todo.objects.filter(user=request.user, date_completed__isnull=False)
             if todo.__len__() == 0 :
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -107,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+#'ar'
 
 TIME_ZONE = 'UTC'
 
@@ -115,6 +116,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
 
 
 # Static files (CSS, JavaScript, Images)
